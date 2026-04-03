50c820913dbe337c47bc31b4797a5639ca2804b3
Juan Jose Vasquez-Mesa <juanjosevasquezmesa@gmail.com>
2026-04-03 12:19:35 -0400

    first commit
    added comments and tested if it is possible to have multiple dynamic blocks

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # initialize database
 # connection string is in the format mysql://user:password@server/database
-conn_str = "mysql://root:iit123@localhost/boatdb"
+conn_str = "mysql://root:cset155@localhost/boatdb"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
@@ -13,7 +13,7 @@ conn = engine.connect()
 # render a file
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') #returns mean if the decorator above runs the system will return this file to the webpage 
 
 
 # remember how to take user inputs?
@@ -27,9 +27,9 @@ def user(name):
 def get_boats():
     # local_session = Session(bind=engine)
     # boats = local_session.query(BoatsModel).all()  # returns all boats
-    boats = conn.execute(text("select * from boats")).all()
+    boats = conn.execute(text("select * from boats")).all() #boats is an array 
     print(boats)
-    return render_template('boats.html', boats=boats[:10])
+    return render_template('boats.html', boats=boats[:10]) # boats is the parameter wiht the argument being an array of the first 10 values in the boats variable outside if this function
 
 
 if __name__ == '__main__':
